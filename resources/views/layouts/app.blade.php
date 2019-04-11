3fea9fb2180ddb2a5ff815c58d8f6f3bda930237
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-04-10 21:42:28 -0500

    Allowing to display the HTTP Client messages in any location

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,6 +72,28 @@
             </div>
         </nav>
 
+        @if(isset($errors) && $errors->any())
+        <div class="alert alert-danger">
+            <button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <ul>
+                @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
+        @if(session()->has('success'))
+        <div class="alert alert-success">
+            <button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <ul>
+                @foreach(session()->get('success') as $message)
+                <li>{{$message}}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
         <main class="py-4">
             @yield('content')
         </main>
