819661ec7ea3ee6a44e93b29a0fce2347f0f2ffd
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-04-10 21:12:29 -0500

    Generating the initial components of the HTTP client interface

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['register' => false, 'reset' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
