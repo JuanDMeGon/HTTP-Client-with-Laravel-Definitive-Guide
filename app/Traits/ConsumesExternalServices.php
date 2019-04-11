d722659508e03dddaa82499e69ae67b592c4ce12
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-04-11 15:39:25 -0500

    Showing the list of products on the main page of the HTTP Client

--- app/Traits/ConsumesExternalServices.php
+++ app/Traits/ConsumesExternalServices.php
@@ -29,7 +29,7 @@ trait ConsumesExternalServices
         $response = $response->getBody()->getContents();
 
         if (method_exists($this, 'decodeResponse')) {
-            $this->decodeResponse($response);
+            $response = $this->decodeResponse($response);
         }
 
         if (method_exists($this, 'checkIfErrorResponse')) {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <h2 class="display-2">Products</h2>
+        </div>
+    </div>
+    <div class="row">
+        @foreach($products as $product)
+            <div class="col-4">
+                <div class="card">
+                    <img src="{{$product->picture}}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$product->title}}</h5>
+                        <p class="card-text">{{$product->details}}</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
 @endsection
