2aab9ad0cdf78f65413e1c0c8bb27e0979547cd0
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-04-23 18:03:45 -0500

    Handling internal exceptions of the HTTP Client

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="row">
+                <div class="col">
+                    <h2 class="display-2">404 Error</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p class="lead">We cannot find this. Please check the URL.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="row">
+                <div class="col">
+                    <h2 class="display-2">500 Error</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p class="lead">We did not expect this. Please try later.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
