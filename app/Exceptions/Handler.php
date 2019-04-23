24273b4cf08263a6efb37c3746c429c9a8230318
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-04-23 17:36:07 -0500

    Preparing the HTTP client to handle failures in requests

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -46,6 +47,32 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof ClientException) {
+            return $this->handleClientException($exception, $request);
+        }
+
         return parent::render($request, $exception);
+    }
+
+    /**
+     * Handle correctly the exceptions when sending requests
+     * @return \Illuminate\Http\Response
+     */
+    protected function handleClientException($request, $exception)
+    {
+        $code = $exception->getCode();
+
+        $response = json_decode($exception->getResponse()->getBody()->getContents());
+        $errorMessage = $response->error;
+
+        switch ($code) {
+            case 'value':
+                # code...
+                break;
+
+            default:
+                # code...
+                break;
+        }
     }
 }
